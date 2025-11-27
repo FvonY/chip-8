a697795f09f3058ff1724669192d46de661f5cb3
Johannes Schulze <schulze.joh@gmail.com>
2025-11-27 21:48:48 +0100

    still alive

--- chip8machine.h
+++ chip8machine.h
@@ -1,19 +1,23 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
-#include "stack.h"
 #include <stdint.h>
+#include "stack.h"
 #define RAM_SIZE 4096
 
 typedef struct {
-  unsigned char display_buffer[256];
-  unsigned char mem[RAM_SIZE];
-  unsigned int pc;
-  uint16_t I;
-  Stack stack;
-  uint8_t delay_timer;
-  uint8_t sound_timer;
-  unsigned char v[16];
+    // Display Buffer
+    unsigned char display_buffer[256];
+    // RAM
+    unsigned char mem[RAM_SIZE];
+    // Program Counter
+    unsigned int pc;
+    // Instruction
+    uint16_t I;
+    Stack stack;
+    uint8_t delay_timer;
+    uint8_t sound_timer;
+    unsigned char v[16];
 } Chip8;
 
 #endif
